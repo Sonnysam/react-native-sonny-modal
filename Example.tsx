9c4f7d2ed4dacb272fa895c5670a0a04fc4d1d1a
sonnysam <samuelagbenyo067@gmail.com>
2024-04-04 19:33:42 +0000

    example file added

---
+++ Example.tsx
@@ -0,0 +1,47 @@
+import { StyleSheet, Text, View, Button } from "react-native";
+import React, { useState } from "react";
+import SonnyModal from "./components/SonnyModal";
+
+export default function App() {
+    const [modalVisible, setModalVisible] = useState(false);
+
+    const openModal = () => {
+        setModalVisible(true);
+    };
+
+    const closeModal = () => {
+        setModalVisible(false);
+    };
+    return (
+        <View
+            style={{
+                flex: 1,
+                justifyContent: "center",
+                alignItems: "center",
+            }}
+        >
+            <Button title="Open Modal" onPress={openModal} />
+            <SonnyModal
+                visible={modalVisible}
+                onClose={closeModal}
+                title="Modal Title"
+                message="
+           Explore our collection of tutorial videos available in multiple
+          languages. Please choose your preferred video language version.
+          Discover step-by-step instructions to enhance your app experience.
+        "
+                animationType="slide"
+                presentationStyle="pageSheet"
+                onDismiss={() => console.log("Modal dismissed")}
+                onShow={() => console.log("Modal shown")}
+                backdropColor="black"
+                backdropOpacity={0.5}
+                swipeDirection="down"
+                onSwipeComplete={() => console.log("Swipe complete")}
+                keyboardAvoiding={true}
+            />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({});
